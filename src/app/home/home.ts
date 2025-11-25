757429739e3d3d27015c0a6cbc58e016f0213a49
nabilhlel <hlelnabil53@gmail.com>
2025-11-25 10:44:25 +0100

    1st

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../task-service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
 export class Home {
 
+  taskForm: FormGroup;
+
+  constructor(private taskService: TaskService, private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: [''],
+      description: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.taskForm.value);
+    this.taskService.addTask(this.taskForm.value.title);
+  }
 }
+
